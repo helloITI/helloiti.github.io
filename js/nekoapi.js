b6c3e8c12e62b4d147a7b0ec5d428fd4194fb718
helloT <hsempin800@g.educaand.es>
2026-03-19 09:41:39 +0100

    Update nekoapi.js

--- js/nekoapi.js
+++ js/nekoapi.js
@@ -2,6 +2,7 @@ async function ld() {
 	// this uses the nekoweb API in order to get any website's stats, such as followers, views, etc.
 	// you can also borrow this code if you wanna add it to your website! (credits not neccessary, but would be nice)
 	// and NO AI was used on this, i basically just looked up tutorials, and thats it.
+	// also uhh i don't think this is neccessary anymore beacuse i swited to github pages.... so.............. idk.
     let ur = 'https://nekoweb.org/api/site/info/hellot.nekoweb.org';
     let ob = await (await fetch(ur)).json();
     let cd = new Date(ob.created_at).toLocaleString();
