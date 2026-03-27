2bfe2f5ebfa581291334ee763fd211e755fe16c2
※⁜ helloT ⁜※ <pnid.hellot@gmail.com>
2026-03-28 00:01:36 +0100

    Update fl.js

--- hellot/seal/fl.js
+++ hellot/seal/fl.js
@@ -4,15 +4,16 @@ import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/9
 // TSTTTTTTTTTTTTTTTTTS PMOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO 😂😂😂
 // i love and hate firebase at the same time.
 // well firebase is easier than... other stuff such as cloudflare, even if it has 1GB of storage its still easy 🥹
-const firebaseConfig = {
-  apiKey: "AIzaSyDGSMqVYXnhuOZdyKXgeQ7KxFhOncHb-ho",
-  authDomain: "mii-storage.firebaseapp.com",
-  databaseURL: "https://mii-storage-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "mii-storage",
-  storageBucket: "mii-storage.firebasestorage.app",
-  messagingSenderId: "974344318870",
-  appId: "1:974344318870:web:3037dc6620f656ef67f7b7"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyBMBAlA_yAXxzeos2xQGrvGRh05UaxomjM",
+    authDomain: "son-im-crine.firebaseapp.com",
+    databaseURL: "https://son-im-crine-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "son-im-crine",
+    storageBucket: "son-im-crine.firebasestorage.app",
+    messagingSenderId: "826626917380",
+    appId: "1:826626917380:web:66e2e78d9d7f92cc25a151",
+    measurementId: "G-T2YGTXMVSF"
+  };
 const app = initializeApp(firebaseConfig); const db = getDatabase(app); const auth = getAuth(app); signInAnonymously(auth);
 const counterRef = ref(db, "tictac");
 let currentClicks = 0;
