7b9f1ac8c848e74d6e26f1614351e269107e1e91
※⁜ helloT ⁜※ <pnid.hellot@gmail.com>
2026-06-18 20:40:42 +0200

    Update ms_bf.js

--- js/ms_bf.js
+++ js/ms_bf.js
@@ -1,32 +1,6 @@
-    const mu = document.getElementById("bg"); // ok mañana 🤣🫱 //
-    let bf = '';
-    window.addEventListener('keydown', (e) => {
-      bf += e.key.toLowerCase();
-      if (bf.length > 10) bf = bf.slice(-10);
-      if (bf.includes('mii')) {
-        mu.src = 'https://helloiti.github.io/assets/seal_song.mp3'; // Ameebo 💜 //
-        mu.loop = true;
-        mu.play().catch(() => {});
-        bf = '';
-      }
-      if (bf.includes('creator')) {
-        mu.src = 'https://helloiti.github.io/assets/0.mp3'; // Petah, the Creator is here. //
-        mu.loop = true;
-        mu.play().catch(() => {});
-        bf = '';
-      }
-      if (bf.includes('armin')) { // this society is so AR AR ARMIN 💜 //
-        window.location.href = "https://helloiti.github.io/assets/How_Did_You_Found_This.mp4"; // ar ar armin //
-      }
-      if (bf.includes('w')) {
-        mu.src = 'https://helloiti.github.io/assets/PEAK.mp3';
-        mu.loop = true;
-        mu.play().catch(() => {});
-        bf = '';
-      }
-    });
-    document.addEventListener("click", function pm() {
-      mu.play().catch(err => {});
-      document.removeEventListener("click", pm);
-    });
-    // poop ♪ //
+const mu = document.getElementById("bg"); let bf = ''; window.addEventListener('keydown', (e) => { bf += e.key.toLowerCase(); if (bf.length > 10) bf = bf.slice(-10);
+if (bf.includes('mii')) { mu.src = 'https://helloiti.github.io/assets/seal_song.mp3'; mu.loop = true; mu.play().catch(() => {}); bf = ''; 
+}if (bf.includes('creator')) { mu.src = 'https://helloiti.github.io/assets/0.mp3'; mu.loop = true; mu.play().catch(() => {}); bf = '';
+}if (bf.includes('armin')) { window.location.href = "https://helloiti.github.io/assets/How_Did_You_Found_This.mp4";
+}if (bf.includes('w')) { mu.src = 'https://helloiti.github.io/assets/PEAK.mp3'; mu.loop = true; mu.play().catch(() => {}); bf = ''; } });
+document.addEventListener("click", function pm() { mu.play().catch(err => {}); document.removeEventListener("click", pm); });
