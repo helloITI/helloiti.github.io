d07bfd0f3aed51659dc52bf2bf926234fc9ed6d6
※⁜ helloT ⁜※ <pnid.hellot@gmail.com>
2026-06-26 00:57:10 +0200

    Update ms_bf.js

--- js/ms_bf.js
+++ js/ms_bf.js
@@ -1,6 +1,7 @@
 const mu = document.getElementById("bg"); let bf = ''; window.addEventListener('keydown', (e) => { bf += e.key.toLowerCase(); if (bf.length > 10) bf = bf.slice(-10);
-if (bf.includes('mii')) { mu.src = 'https://helloiti.github.io/assets/seal_song.mp3'; mu.loop = true; mu.play().catch(() => {}); bf = ''; 
+if (bf.includes('mii')) { mu.src = 'https://helloiti.github.io/assets/mk.mp3'; mu.loop = true; mu.play().catch(() => {}); bf = ''; 
 }if (bf.includes('creator')) { mu.src = 'https://helloiti.github.io/assets/0.mp3'; mu.loop = true; mu.play().catch(() => {}); bf = '';
 }if (bf.includes('armin')) { window.location.href = "https://helloiti.github.io/assets/How_Did_You_Found_This.mp4";
+}if (bf.includes('a')) { mu.src = 'https://helloiti.github.io/assets/mii.mp3'; mu.loop = true; mu.play().catch(() => {}); bf = '';
 }if (bf.includes('w')) { mu.src = 'https://helloiti.github.io/assets/PEAK.mp3'; mu.loop = true; mu.play().catch(() => {}); bf = ''; } });
 document.addEventListener("click", function pm() { mu.play().catch(err => {}); document.removeEventListener("click", pm); });
