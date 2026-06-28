1e923c52e068ee0260ed6d174a2f5ff821c633be
※⁜ helloT ⁜※ <pnid.hellot@gmail.com>
2026-06-28 20:24:30 +0200

    Update sp.js

--- js/sp.js
+++ js/sp.js
@@ -1,10 +1,4 @@
-  const si = document.getElementById("hs");
-  const pop = document.getElementById("pop");
-  function pl() {
-    if (!pop) return;
-    pop.currentTime = 0;
-    pop.play().catch(() => {});
-  }
-  si.addEventListener("mouseenter", pl);
-  si.addEventListener("touchstart", pl);
-  si.addEventListener("click", pl);
+const si = document.getElementById("hs"); const pop = document.getElementById("pop");
+function pl() {
+if (!pop) return;pop.currentTime = 0;pop.play().catch(() => {}); }
+si.addEventListener("mouseenter", pl);si.addEventListener("touchstart", pl);si.addEventListener("click", pl);
