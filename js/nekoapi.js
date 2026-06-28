8ad1effd2a88bd976c05cd5111a1ce18e5f3acc1
※⁜ helloT ⁜※ <pnid.hellot@gmail.com>
2026-06-28 20:23:32 +0200

    Update nekoapi.js

--- js/nekoapi.js
+++ js/nekoapi.js
@@ -2,17 +2,17 @@ async function ld() {
 	// this uses the nekoweb API in order to get any website's stats, such as followers, views, etc.
 	// you can also borrow this code if you wanna add it to your website! (credits not neccessary, but would be nice)
 	// also i don't think this is neccessary anymore beacuse i switched to github pages so idk
-    let ur = 'https://nekoweb.org/api/site/info/hellot.nekoweb.org';
+    let ur = 'https://nekoweb.org/api/site/info/hellot.nekoweb.org'; // you can replace "hellot.nekoweb.org" with your nekoweb website instead
     let ob = await (await fetch(ur)).json();
     let cd = new Date(ob.created_at).toLocaleString();
     let ud = new Date(ob.updated_at).toLocaleString();
     document.getElementById("pt").innerHTML =
-        "<center><h2>※⁜ my website's stats ⁜※</h2></center>" +
-        "<center>⁜ views: " + ob.views + " ⁜</center><br>" +
-        "<center>⁜ followers: " + ob.followers + " ⁜</center><br>" +
-        "<center>⁜ updates: " + ob.updates + " ⁜</center><br>" +
-        "<center>⁜ created at: " + cd + " ⁜</center><br>" +
-        "<center>⁜ last updated at: " + ud + " ⁜</center>";
+        "<center><h2>My website's stats</h2></center>" +
+        "<center>Views: " + ob.views + "</center><br>" +
+        "<center>Followers: " + ob.followers + "</center><br>" +
+        "<center>Updates: " + ob.updates + "</center><br>" +
+        "<center>Created at: " + cd + "</center><br>" +
+        "<center>Last updated at: " + ud + "</center>";
 }
 ld();
 // ok bye
