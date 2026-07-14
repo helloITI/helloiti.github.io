0ea0f25382f4d3ececf4edf3ed112c7e69c18fff
※ helloT ※ <pnid.hellot@gmail.com>
2026-07-14 17:39:14 +0200

    Update ms_bf.js

--- js/ms_bf.js
+++ js/ms_bf.js
@@ -7,6 +7,6 @@ mu.play().then(() => {console.log("playing");}).catch(console.error);bf = "";}
 if (bf.includes("mii")) {ps("https://helloiti.github.io/assets/mk.mp3");
 } else if (bf.includes("creator")) {ps("https://helloiti.github.io/assets/0.mp3");
 } else if (bf.includes("armin")) {location.href = "https://helloiti.github.io/assets/How_Did_You_Found_This.mp4";
-} else if (bf.includes("wiiu")) {ps("https://helloiti.github.io/assets/mii.mp3");
+} else if (bf.includes("3ds")) {ps("https://helloiti.github.io/assets/mii.mp3");
 } else if (bf.includes("w")) {ps("https://helloiti.github.io/assets/PEAK.mp3");}});
 document.addEventListener("click", function pm() {mu.play().catch(console.error);document.removeEventListener("click", pm);});});
