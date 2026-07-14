728374781585881abb37b238238c7910f6860378
※ helloT ※ <pnid.hellot@gmail.com>
2026-07-14 17:33:33 +0200

    Update ms_bf.js

--- js/ms_bf.js
+++ js/ms_bf.js
@@ -5,6 +5,6 @@ function ps(src) {mu.pause();mu.src = src;mu.load();mu.loop = true;mu.play().cat
 if (bf.includes("mii")) {ps("https://helloiti.github.io/assets/mk.mp3");
 } else if (bf.includes("creator")) {ps("https://helloiti.github.io/assets/0.mp3");
 } else if (bf.includes("armin")) {window.location.href = "https://helloiti.github.io/assets/How_Did_You_Found_This.mp4";
-} else if (bf.includes("a")) { ps("https://helloiti.github.io/assets/mii.mp3");
+} else if (bf.includes("wiiu")) { ps("https://helloiti.github.io/assets/mii.mp3");
 } else if (bf.includes("w")) {ps("https://helloiti.github.io/assets/PEAK.mp3");
 }});document.addEventListener("click", function pm() {if (mu) {mu.play().catch(() => {});} document.removeEventListener("click", pm);});
