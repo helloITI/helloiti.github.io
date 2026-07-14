960032aa4619e1e9e37f6494198328ec83f427f4
※ helloT ※ <pnid.hellot@gmail.com>
2026-07-14 17:35:36 +0200

    Update ms_bf.js

--- js/ms_bf.js
+++ js/ms_bf.js
@@ -1,10 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
 const mu = document.getElementById("mu");let bf = "";
-window.addEventListener("keydown", (e) => {
-if (!mu) return;bf += e.key.toLowerCase();if (bf.length > 10) bf = bf.slice(-10);
-function ps(src) {mu.pause();mu.src = src;mu.load();mu.loop = true;mu.play().catch(() => {});bf = "";}
+window.addEventListener("keydown", (e) => {if (!mu) return;
+bf += e.key.toLowerCase();if (bf.length > 10) bf = bf.slice(-10);
+function ps(src) {mu.pause();mu.src = src;mu.load();mu.loop = true;
+mu.play().then(() => {console.log("playing");}).catch(console.error);bf = "";}
 if (bf.includes("mii")) {ps("https://helloiti.github.io/assets/mk.mp3");
 } else if (bf.includes("creator")) {ps("https://helloiti.github.io/assets/0.mp3");
-} else if (bf.includes("armin")) {window.location.href = "https://helloiti.github.io/assets/How_Did_You_Found_This.mp4";
-} else if (bf.includes("wiiu")) { ps("https://helloiti.github.io/assets/mii.mp3");
-} else if (bf.includes("w")) {ps("https://helloiti.github.io/assets/PEAK.mp3");
-}});document.addEventListener("click", function pm() {if (mu) {mu.play().catch(() => {});} document.removeEventListener("click", pm);});
+} else if (bf.includes("armin")) {location.href = "https://helloiti.github.io/assets/How_Did_You_Found_This.mp4";
+} else if (bf.includes("wiiu")) {ps("https://helloiti.github.io/assets/mii.mp3");
+} else if (bf.includes("w")) {ps("https://helloiti.github.io/assets/PEAK.mp3");}});
+document.addEventListener("click", function pm() {mu.play().catch(console.error);document.removeEventListener("click", pm);});});
