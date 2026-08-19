d422065891c600e4fdfdd8347e7c6cd2a57f08dc
※ helloT ※ <pnid.hellot@gmail.com>
2026-08-19 13:27:07 +0200

    Update cl.js

--- hellot/seal/clicker/cl.js
+++ hellot/seal/clicker/cl.js
@@ -1,12 +1,11 @@
 const canvas = document.getElementById('canvas');
 
-const renderer = new THREE.WebGLRenderer({
-  canvas, antialias: window.innerWidth > 768, alpha: true, powerPreference: "high-performance"
-});
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true, alpha: true });
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, window.innerWidth < 768 ? 1.5 : 2));
 renderer.outputColorSpace = THREE.SRGBColorSpace;
+renderer.toneMapping = THREE.NoToneMapping;
 
 const scene = new THREE.Scene();
 
@@ -360,3 +359,5 @@ langSelect.addEventListener('change', (e) => {
   localStorage.setItem('sealLang', e.target.value);
   updateLanguage(e.target.value);
 });
+
+// hi \\
